c3abea36b384dce7f6cd342e70177a941b5f5683
James Droste <james@droste.im>
2016-04-05 03:17:50 -0400

    Starting work on ScoreEngine 2.0, for Lockdown v0

---
+++ scoring/models.py
@@ -0,0 +1,76 @@
+from datetime import datetime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+import sqlalchemy as db
+
+Base = declarative_base()
+
+class Team(Base):
+	__tablename__ = 'teams'
+
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(255), unique=True)
+	enabled = db.Column(db.Boolean)
+
+	def __init__(self, name, enabled=True):
+		self.name = name
+		self.enabled = enabled
+
+class Service(Base):
+	__tablename__ = 'services'
+
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(255), unique=True)
+	check = db.Column(db.String(255))
+	enabled = db.Column(db.Boolean)
+
+	def __init__(self, name, check, enabled=True):
+		self.name = name
+		self.check = check
+		self.enabled = enabled
+
+class TeamService(Base):
+	__tablename__ = 'team_service'
+
+	id = db.Column(db.Integer, primary_key=True)
+	
+	team_id = db.Column(db.Integer, db.ForeignKey('teams.id'))
+	team = relationship('Team')
+	
+	service_id = db.Column(db.Integer, db.ForeignKey('services.id'))
+	service = relationship('Service')
+
+	key = db.Column(db.String(255))
+	value = db.Column(db.Text)
+	edit = db.Column(db.Boolean)
+	order = db.Column(db.Integer)
+
+	def __int__(self, team, service, key, value, edit=True, order=0):
+		self.team = team
+		self.service = service
+		self.key = key
+		self.value = value
+		self.edit = edit
+		self.order = order
+
+class Check(Base):
+	__tablename__ = 'checks'
+
+	id = db.Column(db.Integer, primary_key=True)
+
+	team_id = db.Column(db.Integer, db.ForeignKey('teams.id'))
+	team = relationship('Team')
+
+	service_id = db.Column(db.Integer, db.ForeignKey('services.id'))
+	service = relationship('Service')
+
+	time = db.Column(db.DateTime)
+	passed = db.Column(db.Boolean)
+	output = db.Column(db.Text)
+
+	def __init__(self, team, service, passed, output):
+		self.team = team
+		self.service = service
+		self.passed = passed
+		self.output = output
+		self.time = datetime.utcnow()
