d7f0c9c5f69461afa60ca69564f225d701516eff
Stephen James <stephenorjames@gmail.com>
2017-04-08 17:08:27 -0400

    Increase the DB session pool size

--- scoring/__init__.py
+++ scoring/__init__.py
@@ -3,6 +3,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 
 # Init SQLAlchemy
-engine = create_engine(config['DATABASE_URI'])
+engine = create_engine(config['DATABASE_URI'], pool_size=40, max_overflow=60)
 SessionMaker = sessionmaker(bind=engine)
 Session = scoped_session(SessionMaker)
