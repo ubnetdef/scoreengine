a1c61b38efa443d4ced0db69d1d9ec3062c2fad5
James Droste <james@droste.im>
2018-04-01 17:54:44 -0700

    http: Add (untested) check_gitlab check

--- scoring/checks/http.py
+++ scoring/checks/http.py
@@ -14,6 +14,9 @@ http_config = {
 
 	'wordpress_login': 'wp-login.php',
 	'wordpress_cookie': 'wordpress_logged_in_',
+
+	'gitlab_login': 'users/sign_in',
+	'gitlab_cookie': '_gitlab_session',
 }
 # /DEFAULTS
 
@@ -80,7 +83,7 @@ def check_wordpress(check, data):
 		check.addOutput("Connected!")
 
 		# Load the login page
-		login_url = "http://{HOST}:{PORT}/{wordpress_login}".format(wordpress_login=http_config["wordpress_login"], **data)
+		login_url = "http://{HOST}:{PORT}/{login}".format(login=http_config["wordpress_login"], **data)
 		login_payload = {
 			'log': data["USER"],
 			'pwd': data["PASS"]
@@ -107,6 +110,73 @@ def check_wordpress(check, data):
 		has_login_cookie = False
 		for c in session.cookies:
 			if http_config["wordpress_cookie"] in c.name:
+				has_login_cookie = True
+
+		if not has_login_cookie:
+			check.addOutput("ERROR: Logged in cookie not set.")
+			return
+
+		check.addOutput("Logged in!")
+
+		# It passed all our check
+		check.setPassed()
+		check.addOutput("Check successful!")
+	except Exception as e:
+		check.addOutput("ERROR: {}: {}".format(type(e).__name__, e))
+
+	return
+
+def check_gitlab(check, data):
+	check.addOutput("ScoreEngine: {} Check\n".format(check.getServiceName()))
+	check.addOutput("EXPECTED: Ability to use the gitlab website")
+	check.addOutput("OUTPUT:\n")
+	check.addOutput("Starting check...")
+
+	try:
+		# Time the start of the request
+		reqStart = datetime.now()
+
+		# Connect to the website
+		check.addOutput("Connecting to http://{HOST}:{PORT}".format(**data))
+		session = requests.Session()
+		req = session.get("http://{HOST}:{PORT}".format(**data), timeout=http_config["timeout"], headers=http_headers)
+
+		if req.status_code != 200:
+			check.addOutput("ERROR: Page returned status code {}".format(req.status_code))
+			return
+
+		check.addOutput("Connected!")
+
+		# Load the login page
+		check.addOutput("Loading login page")
+		login_url = "http://{HOST}:{PORT}/{login}".format(login=http_config["gitlab_login"], **data)
+		req = session.get(login_url, timeout=http_config["timeout"], headers=http_headers)
+		matches = re.search('name="authenticity_token" value="([^"]+)"', req.content)
+
+		if not matches:
+			check.addOutput("ERROR: Login page did not contain needed information")
+			return
+
+		# Attempt a login
+		login_payload = {
+			'user[login]': data["USER"],
+			'user[password]': data["PASS"],
+			'authenticity_token': matches.group(1)
+		}
+
+		check.addOutput("Attempting login")
+		req = session.get(login_url, timeout=http_config["timeout"], headers=http_headers)
+
+		if req.status_code != 200:
+			check.addOutput("ERROR: Page returned status code {}".format(req.status_code))
+			return
+
+		check.addOutput("Loaded!")
+
+		# Check the cookies
+		has_login_cookie = False
+		for c in session.cookies:
+			if http_config["gitlab_cookie"] in c.name:
 				has_login_cookie = True
 
 		if not has_login_cookie:
